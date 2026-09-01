923355978482257d5f737a69bb6afd89f86763d0
Joel <joel@typefasterjoel.com>
2026-08-31 23:01:56 -0700

    Adding netlify and removing crap files.

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,18 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
-	resolve: { tsconfigPaths: true },
-	plugins: [
-		devtools(),
-		nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-	],
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    netlify(),
+  ],
 });
 
 export default config;
